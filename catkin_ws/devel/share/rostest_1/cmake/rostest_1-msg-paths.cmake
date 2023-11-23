dacd5c2cbb10e3f4c198037219c7031d271c37a0
KK <kishorkumar2491@gmail.com>
2023-11-23 12:07:38 +0530

    Delete catkin_ws/devel directory

--- catkin_ws/devel/share/rostest_1/cmake/rostest_1-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(rostest_1_MSG_INCLUDE_DIRS "")
-set(rostest_1_MSG_DEPENDENCIES std_msgs)
--- catkin_ws/devel/share/subtocamera/cmake/subtocamera-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(subtocamera_MSG_INCLUDE_DIRS "")
-set(subtocamera_MSG_DEPENDENCIES sensor_msgs)
